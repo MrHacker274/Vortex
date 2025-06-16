504f0099a301924d815ad7b3c7bbeda809549dcb
MrHacker274 <mrhacker222000@gmail.com>
2025-06-16 11:22:09 +0530

    Update infobot.py

--- infobot.py
+++ infobot.py
@@ -139,17 +139,9 @@ def is_user_paid(user_id: int) -> bool:
     except:
         pass
     return False
-
-# ===== Dummy Placeholder: Convert ID to Username =====
 def get_username_from_user_id(user_id):
-    # This requires an external API or database access in reality.
-    # For now, return dummy data:
     return f"user{user_id}"
-
-# ===== Dummy Placeholder: Fetch Instagram Info =====
 def fetch_instagram_info(username):
-    # Placeholder for actual IG scraping or API call.
-    # Return sample HTML-formatted data:
     return (
         f"<b>👤 Username:</b> @{username}\n"
         f"<b>🧾 Bio:</b> Sample bio for {username}\n"
@@ -310,6 +302,21 @@ import user_agent
 import requests
 import random
 import re
+cokANDdata=requests.get('https://login.aol.com/account/create',headers={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0','accept-language': 'en-US,en;q=0.9',})
+AS=cokANDdata.cookies.get_dict()['AS']
+A1=cokANDdata.cookies.get_dict()['A1']
+A3=cokANDdata.cookies.get_dict()['A3']
+A1S=cokANDdata.cookies.get_dict()['A1S']
+specData=cokANDdata.text.split('''name="attrSetIndex">
+        <input type="hidden" value="''')[1].split(f'" name="specData">')[0]
+specId=cokANDdata.text.split('''name="browser-fp-data" id="browser-fp-data" value="" />
+        <input type="hidden" value="''')[1].split(f'" name="specId">')[0]
+crumb=cokANDdata.text.split('''name="cacheStored">
+        <input type="hidden" value="''')[1].split(f'" name="crumb">')[0]
+sessionIndex=cokANDdata.text.split('''"acrumb">
+        <input type="hidden" value="''')[1].split(f'" name="sessionIndex">')[0]
+acrumb=cokANDdata.text.split('''name="crumb">
+        <input type="hidden" value="''')[1].split(f'" name="acrumb">')[0]
 def check_aol_username(username):
     def check_availability_from_text(response_text):
         response_text = response_text.strip()
@@ -335,34 +342,36 @@ def check_aol_username(username):
 
     # [YOUR COOKIES AND HEADERS HERE - ALREADY OK IN YOUR SCRIPT]
     cookies = {
-    'GUC': 'AQEBCAFoSRdoeEIdQwQy&s=AQAAACCB4HIN&g=aEfHog',
-    'A1': 'd=AQABBMaWOWgCEL-FOLxgsvlB2W8Ovy55PwIFEgEBCAEXSWh4aFkPyyMA_eMDAAcIxpY5aC55PwI&S=AQAAApTnrAS4ma9896ODHJgQIvo',
-    'A3': 'd=AQABBMaWOWgCEL-FOLxgsvlB2W8Ovy55PwIFEgEBCAEXSWh4aFkPyyMA_eMDAAcIxpY5aC55PwI&S=AQAAApTnrAS4ma9896ODHJgQIvo',
-    'A1S': 'd=AQABBMaWOWgCEL-FOLxgsvlB2W8Ovy55PwIFEgEBCAEXSWh4aFkPyyMA_eMDAAcIxpY5aC55PwI&S=AQAAApTnrAS4ma9896ODHJgQIvo',
-    'AS': 'v=1&s=slDB7NJy&d=A684d5b80|N1zUs.X.2SpLwKEm_MfqqESW3.tnIEX8Uc81Ahdjxc7kpH1C8SVTzcQ8IbcuXRwEWTL8BVW7zsi1clfpmaQTYdu3DT7sy_8wo.dM8mOaCPfASt2xEBrCrwV_var4R0gK2HAcIMVMdC5vO8m3vRcnueOQp92ManftN5JTcUV6bbo16_34GrP1nq4FzImjBSC0ZF_I33Gs.0AVYiVhtx.v2uQJiUy8_nvwK9oCxQQfCSJq6thHhkSBGc5aRyiLvOE0SmXQXUJhQLVW4qVaAf0NPD4iM7VWRcc.h6Bd7W8a1JoqPFK5bPPTZBOOzcCBHL4kZwn.G2BBwwV_srAB_PrU490Ig9xQnIx8Bs8mWkbAYjTw_37bX.ZV6n2ITQStTYIE5pQT.K2CKfSSvhoFE0i1FNKV.N7TKeZlUUNFIfttIgZMPipDcmDjZX.VHVQvxZYqPYqIfTQZ4JOnEXZMIUKC3GwGL31MDNraMJWhXSX9O4dsbgI0xUEwPa1Lo0IikLp_Kfv1S0kwXMFpEHKmQlfJ1zLCt_NdL6cX.naYEMBoFNSTkTubX__xdUL092G85DAE8gbiaflkUWAbilHEbzpTH1mXrwvi8w47IjUmgtXMtauqI4cIUzX5AibIQtGBK0wSzSp7vbOvHTEUMDuzmTJHne58ao3lnf_uY2PD4KaX9wAlDcwvd16PQbZrm16zMkHelsRnwlqKkUvCXm_fDTfbVPw5ON7PU1khvZ8IE1NljTwsTgm7IxphpN.Aa_rHEI5LQlBZJvmX3JD16diA440y0xKkhga4MLORLB.V2MqOOEyHsa4lasLq5RT_aIU.hC1pJvFeDIzwHXMjNriXKGZCTMsCNem_SbA8slLypvbdvjqtFIE3U49Z.biU6GugoEwF3oHxndMSIkrh8Ue_RRrhBDfDYCdqvG3xc8vif3Wy0dkUMhMlDOkXheqEbxnBhqcXptSzERz3rVwakY4f3C8RPLch~A|B684d5b83|QPRRaRb.2Trs3KMa58QlxH9uCIkekIayoFyJT3btG2ZfSgERVvIydlJVl8vmpSH6Ufrt77jSWeX2Q7cGR45J69oyX.c0SfpPrd5OJGEHwgc07RqV1adsTcZh.M8P.Dwk8QDKgNdAvNgl7DizVfUDoAzreYNtPJwrEfQwIpxqsNhpmC0KtuADwN_niTqrwNcog0udLVMIhKLvBVQKWSh9j1Xxi0ICCkzpguXSestAh0tFc_srV8B2jaGuEHqlcOF_rOcseLJSVvWpl.6GtymXrGyaVaPSh2ZWIqtHNMDopPyvzu.t4PfX2AND1duCb_0j6.l0FZk8uXy_Jpzioajf7ejHJaoIZTv30oPYNuMUD_eEOdxd4JjXz5_bYaoYnnpSZeKEC0HZ3iTpBlpZ1S6ydQNGE1sK4zCi0Q0rRkaRxppo3Hqsx4KSgb4oth99r9DUNEOWzvCAP94oVvv0MV_sVqojqnScf2ERIRMnYQJYfluA50S4t7C4DXpbpJ2yADoliExKVl03DdEguLIjylOxCu6mOkji6h7iQfWpFeuk7R06M.ECC_r2KvNFiYNNviExVyRVVU2d56qa88PUYlRtUXEye02M3Isoa1_CZSnn9sArr0JX5A55OwawDBmij3ZL198gycrwtn159txpCfIzYTvNy_pNUyavGzSVhxZ7VPf9I2_LwXgzqSyen4iOY5_QzyhIaI.yP2FQR8Zzuh115ip7ZLSPR6Zb1NK5BGgci7BFtlzT2waoS0LE0u.ad96brn4HdsdMqJpr4BsueuP6KYxXy9lC4dB9MLNxtInq1U7sQq6D2o6lYlM7H4uZ5M1CUxfiuTl_cqp8ZY6SugyApCEDtrJOwrZbndbRVfU.vPQzlR5ip7oEyvOd7PRD7vFFHXQme8unXeSwzruXQfvjJ3wZWvup4pVR3.QzdApzvfzADrlp5st2ntEExOAe1JWdrfAeiRlk30q1Mbra4ntW1L3IxBZ_P74hrLlJJPED8BWRJDf6rnF0TWH4Zn_tVNAl61TCZuV_eE7XZSj9ZgYUXzw2aP6X_OxqE74VJx.M5.0fmAroA6zGi9uNZ72204gjeTP33F0oBvaHbQ7jM4lYSociD4qoRPSa61_VmF1zwf_Ys6Ebcp_RZ2KlmKgl0sjS6isEGzTAdg--~A',
+    'gpp': 'DBAA',
+        'gpp_sid': '-1',
+        'A1':A1,
+        'A3':A3,
+        'A1S':A1S,
+        '__gads': 'ID=c0M0fd00676f0ea1:T='+'4'+':RT='+'5'+':S=ALNI_MaEGaVTSG6nQFkSJ-RnxSZrF5q5XA',
+        '__gpi': 'UID=00000cf0e8904e94:T='+'7'+':RT='+'6'+':S=ALNI_MYCzPrYn9967HtpDSITUe5Z4ZwGOQ',
+        'cmp': 't='+'0'+'&j=0&u=1---',
+        'AS': AS,
     }
     headers = {
-    'accept': '*/*',
-    'accept-language': 'en-US,en;q=0.6',
-    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'origin': 'https://login.yahoo.com',
-    'priority': 'u=1, i',
-    'referer': 'https://login.yahoo.com/account/create?.lang=en-US&src=homepage&activity=ybar-signin&pspid=2023538075&.done=https%3A%2F%2Fwww.yahoo.com%2F&specId=yidregsimplified&done=https%3A%2F%2Fwww.yahoo.com%2F',
-    'sec-ch-ua': '"Brave";v="137", "Chromium";v="137", "Not/A)Brand";v="24"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'sec-gpc': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
-    'x-requested-with': 'XMLHttpRequest',
-    # 'cookie': 'GUC=AQEBCAFoSRdoeEIdQwQy&s=AQAAACCB4HIN&g=aEfHog; A1=d=AQABBMaWOWgCEL-FOLxgsvlB2W8Ovy55PwIFEgEBCAEXSWh4aFkPyyMA_eMDAAcIxpY5aC55PwI&S=AQAAApTnrAS4ma9896ODHJgQIvo; A3=d=AQABBMaWOWgCEL-FOLxgsvlB2W8Ovy55PwIFEgEBCAEXSWh4aFkPyyMA_eMDAAcIxpY5aC55PwI&S=AQAAApTnrAS4ma9896ODHJgQIvo; A1S=d=AQABBMaWOWgCEL-FOLxgsvlB2W8Ovy55PwIFEgEBCAEXSWh4aFkPyyMA_eMDAAcIxpY5aC55PwI&S=AQAAApTnrAS4ma9896ODHJgQIvo; AS=v=1&s=slDB7NJy&d=A684d5b80|N1zUs.X.2SpLwKEm_MfqqESW3.tnIEX8Uc81Ahdjxc7kpH1C8SVTzcQ8IbcuXRwEWTL8BVW7zsi1clfpmaQTYdu3DT7sy_8wo.dM8mOaCPfASt2xEBrCrwV_var4R0gK2HAcIMVMdC5vO8m3vRcnueOQp92ManftN5JTcUV6bbo16_34GrP1nq4FzImjBSC0ZF_I33Gs.0AVYiVhtx.v2uQJiUy8_nvwK9oCxQQfCSJq6thHhkSBGc5aRyiLvOE0SmXQXUJhQLVW4qVaAf0NPD4iM7VWRcc.h6Bd7W8a1JoqPFK5bPPTZBOOzcCBHL4kZwn.G2BBwwV_srAB_PrU490Ig9xQnIx8Bs8mWkbAYjTw_37bX.ZV6n2ITQStTYIE5pQT.K2CKfSSvhoFE0i1FNKV.N7TKeZlUUNFIfttIgZMPipDcmDjZX.VHVQvxZYqPYqIfTQZ4JOnEXZMIUKC3GwGL31MDNraMJWhXSX9O4dsbgI0xUEwPa1Lo0IikLp_Kfv1S0kwXMFpEHKmQlfJ1zLCt_NdL6cX.naYEMBoFNSTkTubX__xdUL092G85DAE8gbiaflkUWAbilHEbzpTH1mXrwvi8w47IjUmgtXMtauqI4cIUzX5AibIQtGBK0wSzSp7vbOvHTEUMDuzmTJHne58ao3lnf_uY2PD4KaX9wAlDcwvd16PQbZrm16zMkHelsRnwlqKkUvCXm_fDTfbVPw5ON7PU1khvZ8IE1NljTwsTgm7IxphpN.Aa_rHEI5LQlBZJvmX3JD16diA440y0xKkhga4MLORLB.V2MqOOEyHsa4lasLq5RT_aIU.hC1pJvFeDIzwHXMjNriXKGZCTMsCNem_SbA8slLypvbdvjqtFIE3U49Z.biU6GugoEwF3oHxndMSIkrh8Ue_RRrhBDfDYCdqvG3xc8vif3Wy0dkUMhMlDOkXheqEbxnBhqcXptSzERz3rVwakY4f3C8RPLch~A|B684d5b83|QPRRaRb.2Trs3KMa58QlxH9uCIkekIayoFyJT3btG2ZfSgERVvIydlJVl8vmpSH6Ufrt77jSWeX2Q7cGR45J69oyX.c0SfpPrd5OJGEHwgc07RqV1adsTcZh.M8P.Dwk8QDKgNdAvNgl7DizVfUDoAzreYNtPJwrEfQwIpxqsNhpmC0KtuADwN_niTqrwNcog0udLVMIhKLvBVQKWSh9j1Xxi0ICCkzpguXSestAh0tFc_srV8B2jaGuEHqlcOF_rOcseLJSVvWpl.6GtymXrGyaVaPSh2ZWIqtHNMDopPyvzu.t4PfX2AND1duCb_0j6.l0FZk8uXy_Jpzioajf7ejHJaoIZTv30oPYNuMUD_eEOdxd4JjXz5_bYaoYnnpSZeKEC0HZ3iTpBlpZ1S6ydQNGE1sK4zCi0Q0rRkaRxppo3Hqsx4KSgb4oth99r9DUNEOWzvCAP94oVvv0MV_sVqojqnScf2ERIRMnYQJYfluA50S4t7C4DXpbpJ2yADoliExKVl03DdEguLIjylOxCu6mOkji6h7iQfWpFeuk7R06M.ECC_r2KvNFiYNNviExVyRVVU2d56qa88PUYlRtUXEye02M3Isoa1_CZSnn9sArr0JX5A55OwawDBmij3ZL198gycrwtn159txpCfIzYTvNy_pNUyavGzSVhxZ7VPf9I2_LwXgzqSyen4iOY5_QzyhIaI.yP2FQR8Zzuh115ip7ZLSPR6Zb1NK5BGgci7BFtlzT2waoS0LE0u.ad96brn4HdsdMqJpr4BsueuP6KYxXy9lC4dB9MLNxtInq1U7sQq6D2o6lYlM7H4uZ5M1CUxfiuTl_cqp8ZY6SugyApCEDtrJOwrZbndbRVfU.vPQzlR5ip7oEyvOd7PRD7vFFHXQme8unXeSwzruXQfvjJ3wZWvup4pVR3.QzdApzvfzADrlp5st2ntEExOAe1JWdrfAeiRlk30q1Mbra4ntW1L3IxBZ_P74hrLlJJPED8BWRJDf6rnF0TWH4Zn_tVNAl61TCZuV_eE7XZSj9ZgYUXzw2aP6X_OxqE74VJx.M5.0fmAroA6zGi9uNZ72204gjeTP33F0oBvaHbQ7jM4lYSociD4qoRPSa61_VmF1zwf_Ys6Ebcp_RZ2KlmKgl0sjS6isEGzTAdg--~A',
+    'authority': 'login.aol.com',
+        'accept': '*/*',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'origin': 'https://login.aol.com',
+        'referer': f'https://login.aol.com/account/create?specId={specId}&done=https%3A%2F%2Fwww.aol.com',
+        'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0',
+        'x-requested-with': 'XMLHttpRequest',
     }
     params = {
         'validateField': 'userId',
     }
-    data = f'browser-fp-data=%7B%22language%22%3A%22en-US%22%2C%22colorDepth%22%3A24%2C%22deviceMemory%22%3A1%2C%22pixelRatio%22%3A1%2C%22hardwareConcurrency%22%3A4%2C%22timezoneOffset%22%3A-330%2C%22timezone%22%3A%22Asia%2FCalcutta%22%2C%22sessionStorage%22%3A1%2C%22localStorage%22%3A1%2C%22indexedDb%22%3A1%2C%22cpuClass%22%3A%22unknown%22%2C%22platform%22%3A%22Win32%22%2C%22doNotTrack%22%3A%22unknown%22%2C%22plugins%22%3A%7B%22count%22%3A4%2C%22hash%22%3A%22ed22a97edc37a74300379ae8e012c683%22%7D%2C%22canvas%22%3A%22canvas%20winding%3Ayes~canvas%22%2C%22webgl%22%3A1%2C%22webglVendorAndRenderer%22%3A%22Google%20Inc.%20(Intel)~ANGLE%20(Intel%2C%20Intel(R)%20HD%20Graphics%20Direct3D9Ex%20vs_3_0%20ps_3_0%2C%20igdumd64.dll)%22%2C%22adBlock%22%3A0%2C%22hasLiedLanguages%22%3A0%2C%22hasLiedResolution%22%3A0%2C%22hasLiedOs%22%3A0%2C%22hasLiedBrowser%22%3A0%2C%22touchSupport%22%3A%7B%22points%22%3A0%2C%22event%22%3A0%2C%22start%22%3A0%7D%2C%22fonts%22%3A%7B%22count%22%3A29%2C%22hash%22%3A%2290ead5fa1a480b09b5b0239bd273a529%22%7D%2C%22audio%22%3A%22123.72706087233382%22%2C%22resolution%22%3A%7B%22w%22%3A%221680%22%2C%22h%22%3A%221050%22%7D%2C%22availableResolution%22%3A%7B%22w%22%3A%221050%22%2C%22h%22%3A%221680%22%7D%2C%22ts%22%3A%7B%22serve%22%3A1749813763212%2C%22render%22%3A1749813763962%7D%7D&specId=yidregsimplified&context=REGISTRATION&cacheStored=&crumb=Xit65ahjkHJ6bNHYETSZQ&acrumb=slDB7NJy&sessionIndex=Qg--&done=https%3A%2F%2Fwww.yahoo.com%2F&googleIdToken=&authCode=&attrSetIndex=0&specData=&deviceCapability=%7B%22pa%22%3A%7B%22status%22%3Afalse%7D%2C%22isWebAuthnSupported%22%3Atrue%7D&tos0=oath_freereg%7Cin%7Cen-IN&multiDomain=&asId=0be85269-e438-4aac-b0cc-eda9f4d14b27&fingerprintCaptured=&firstName=ansh&lastName=zuck&userid-domain=yahoo&userId={username}&yidDomainDefault=yahoo.com&yidDomain=yahoo.com&password=&mm=&dd=&yyyy=&signup='
+    data = f'browser-fp-data=%7B%22language%22%3A%22en-US%22%2C%22colorDepth%22%3A24%2C%22deviceMemory%22%3A8%2C%22pixelRatio%22%3A1%2C%22hardwareConcurrency%22%3A4%2C%22timezoneOffset%22%3A-60%2C%22timezone%22%3A%22Africa%2FCasablanca%22%2C%22sessionStorage%22%3A1%2C%22localStorage%22%3A1%2C%22indexedDb%22%3A1%2C%22cpuClass%22%3A%22unknown%22%2C%22platform%22%3A%22Win32%22%2C%22doNotTrack%22%3A%22unknown%22%2C%22plugins%22%3A%7B%22count%22%3A5%2C%22hash%22%3A%222c14024bf8584c3f7f63f24ea490e812%22%7D%2C%22canvas%22%3A%22canvas%20winding%3Ayes~canvas%22%2C%22webgl%22%3A1%2C%22webglVendorAndRenderer%22%3A%22Google%20Inc.%20(Intel)~ANGLE%20(Intel%2C%20Intel(R)%20HD%20Graphics%204000%20(0x00000166)%20Direct3D11%20vs_5_0%20ps_5_0%2C%20D3D11)%22%2C%22adBlock%22%3A0%2C%22hasLiedLanguages%22%3A0%2C%22hasLiedResolution%22%3A0%2C%22hasLiedOs%22%3A0%2C%22hasLiedBrowser%22%3A0%2C%22touchSupport%22%3A%7B%22points%22%3A0%2C%22event%22%3A0%2C%22start%22%3A0%7D%2C%22fonts%22%3A%7B%22count%22%3A33%2C%22hash%22%3A%22edeefd360161b4bf944ac045e41d0b21%22%7D%2C%22audio%22%3A%22124.04347527516074%22%2C%22resolution%22%3A%7B%22w%22%3A%221600%22%2C%22h%22%3A%22900%22%7D%2C%22availableResolution%22%3A%7B%22w%22%3A%22860%22%2C%22h%22%3A%221600%22%7D%2C%22ts%22%3A%7B%22serve%22%3A1704793094844%2C%22render%22%3A1704793096534%7D%7D&specId={specId}&cacheStored=&crumb={crumb}&acrumb={acrumb}&sessionIndex={sessionIndex}&done=https%3A%2F%2Fwww.aol.com&googleIdToken=&authCode=&attrSetIndex=0&specData={specData}&multiDomain=&tos0=oath_freereg%7Cus%7Cen-US&firstName=zuck&lastName=zuck&userid-domain=yahoo&userId={username}&password=&mm=&dd=&yyyy=&signup='
 
 
     response = requests.post(
@@ -614,7 +623,6 @@ def send_recovery_request(email_or_username):
         ("Method 6", method_6),
         ("Method 7", method_7)
     ]
-
     for name, method in methods:
         try:
             result = method(email_or_username)
@@ -626,11 +634,8 @@ def send_recovery_request(email_or_username):
         except Exception as e:
             print(f"[⚠️] {name} raised an exception: {e}")
             continue
-
     return ["No Reset"]
-
 ua = UserAgent()
-
 def method_1(email_or_username):
     cookies = {
         'csrftoken': 'gpexs0wL6nxpdY955MzDDX',
@@ -1048,6 +1053,27 @@ def aol(update: Update, context: CallbackContext):
             parse_mode='Markdown'
         )
 
+def yahoo(update: Update, context: CallbackContext):
+    if not context.args:
+        update.message.reply_text("Usage: /aol <username>")
+        return
+
+    username = context.args[0]
+    if not is_valid_username(username):
+        update.message.reply_text("Username can only contain letters, numbers, underscores (_) and dots (.)")
+        return
+
+    update.message.chat.send_action(ChatAction.TYPING)
+    result = check_yahoo(username)
+
+    if result is None:
+        update.message.reply_text("❌ Couldn't check the username right now. Try again later.")
+    else:
+        update.message.reply_text(
+            f"🔎 Username *{username}@YAHOO.com* is {result}",
+            parse_mode='Markdown'
+        )
+
 # === Gmail ===
 def gmail(update: Update, context: CallbackContext):
     if not context.args:
@@ -1123,6 +1149,7 @@ def main():
     dp.add_handler(CommandHandler("reset", reset_command))
     dp.add_handler(CommandHandler(["info", "infonum"], handle_info_command))
     dp.add_handler(CommandHandler("aol", aol))
+    dp.add_handler(CommandHandler("yahoo", yahoo))
     dp.add_handler(CommandHandler("gmail", gmail))
     dp.add_handler(CommandHandler("subscription", subscription_command))
     dp.add_handler(CommandHandler("hotmail", hotmail))
